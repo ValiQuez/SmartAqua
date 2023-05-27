055db1d7ce41df574a568d4d958bf48e041b59b4
Denis <n01422583@humbermail.ca>
2023-05-26 20:23:37 -0400

    Initial Commit.

---
+++ app/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.buckleupinc.it.smartaqua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
