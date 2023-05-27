1582b1b6cdb0f6e5f8de9877e42c5da7ea6204ad
Paolo Adrian Quezon <n01424883@humbermail.ca>
2023-05-26 21:02:19 -0400

    Updated mainactivity comments

--- app/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
+++ app/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
@@ -1,8 +1,8 @@
-//CENG-322-0NA Group 6
-//Denis Shwaloff n01422583
-//Alvaro Rodrigo Chavez Moya N01455107
-//Paolo Quezon
-//Nicholas Dibiase
+//CENG-322-0NA: Group 6
+//Denis Shwaloff - N01422583
+//Alvaro Rodrigo Chavez Moya - N01455107
+//Paolo Adrian Quezon - N01424883
+//Nicholas Dibiase - N01367109
 
 package ca.buckleupinc.it.smartaqua;
 
