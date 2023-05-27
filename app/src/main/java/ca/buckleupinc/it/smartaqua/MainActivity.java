b53d2c15d8c8aed5933c89e8c56f05da5dad12a0
Denis <n01422583@humbermail.ca>
2023-05-26 20:42:36 -0400

    Add comment with class section and full names of all team members

--- app/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
+++ app/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
@@ -1,3 +1,9 @@
+//CENG-322-0NA Group 6
+//Denis Shwaloff n01422583
+//Alvaro Rodrigo Chavez Moya
+//Paolo Quezon
+//Nicholas Dibiase
+
 package ca.buckleupinc.it.smartaqua;
 
 import androidx.appcompat.app.AppCompatActivity;
