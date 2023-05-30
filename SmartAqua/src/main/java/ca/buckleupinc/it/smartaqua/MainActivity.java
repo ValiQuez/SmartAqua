1a96ed5482bae5738403e092ae2754c86bd1db10
AlvaroChavez5017 <rodrigochv1605@gmail.com>
2023-05-30 19:07:59 -0400

    Finished the functionality of the Navigation Drawer and fixed comments format.

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
@@ -1,20 +1,44 @@
-//CENG-322-0NA: Group 6
-//Denis Shwaloff - N01422583
-//Alvaro Rodrigo Chavez Moya - N01455107
-//Paolo Adrian Quezon - N01424883
-//Nicholas Dibiase - N01367109
+/*  CENG-322-0NA: Group 6
+    Denis Shwaloff - N01422583
+    Alvaro Rodrigo Chavez Moya - N01455107
+    Paolo Adrian Quezon - N01424883
+    Nicholas Dibiase - N01367109            */
 
 package ca.buckleupinc.it.smartaqua;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.SmartAquaToolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.SmartAquaDrawerLayout);
+        NavigationView navigationView = findViewById(R.id.SmartAquaNavigationView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.SmartAquaFragmentSection, new SmartAquaHome()).commit();
+        }
     }
 }
+
+
