af319a793e2e294b6269b4f7b4c54fb57c5a726b
Paolo Adrian Quezon <n01424883@humbermail.ca>
2023-05-30 22:35:21 -0400

    Setup the splash screen

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaSplash.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaSplash.java
@@ -1,4 +1,34 @@
+/*  CENG-322-0NA: Group 6
+    Denis Shwaloff - N01422583
+    Alvaro Rodrigo Chavez Moya - N01455107
+    Paolo Adrian Quezon - N01424883
+    Nicholas Dibiase - N01367109            */
+
 package ca.buckleupinc.it.smartaqua;
 
-public class SmartAquaSplash {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SmartAquaSplash extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 5000; // Splash screen delay in milliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay the execution of the next activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after the splash delay
+                Intent intent = new Intent(SmartAquaSplash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
 }
