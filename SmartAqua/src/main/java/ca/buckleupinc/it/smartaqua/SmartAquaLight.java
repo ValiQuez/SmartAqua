0ceabefa0f2bc1680208e64d9c0441093e7f5152
Nicholas <nickworks679@gmail.com>
2023-07-06 00:03:15 -0400

    Update landscape layout screen and implementation for Light screen

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaLight.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaLight.java
@@ -20,70 +20,24 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SmartAquaLight#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SmartAquaLight extends Fragment {
 
     private Switch switchlightButton;
     private TextView notificationTextView;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SmartAquaLight() {
 
     }
 
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SmartAquaLight.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SmartAquaLight newInstance(String param1, String param2) {
-        SmartAquaLight fragment = new SmartAquaLight();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view;
-
-
-
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            view = inflater.inflate(R.layout.fragment_smart_aqua_light_land, container, false);
-        }
-        else {
-            view = inflater.inflate(R.layout.fragment_smart_aqua_light, container, false);
-        }
+        view = inflater.inflate(R.layout.fragment_smart_aqua_light, container, false);
 
         // Required empty public constructor
         switchlightButton = view.findViewById(R.id.switchlightButton);
