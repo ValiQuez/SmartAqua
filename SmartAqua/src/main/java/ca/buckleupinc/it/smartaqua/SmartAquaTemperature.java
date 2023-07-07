663a3ae01e8e81dbfb77ea185f0e847a86aa196a
Nicholas <nickworks679@gmail.com>
2023-07-07 14:33:03 -0400

    Implemented the functionailty of the temperature seek bar to show the accurate temperature and also updated the strings in english and french to not have hardcoded text

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperature.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperature.java
@@ -11,68 +11,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SmartAquaTemperature#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SmartAquaTemperature extends Fragment {
+    SeekBar seekBar;
+    TextView textView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+
 
     public SmartAquaTemperature() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SmartAquaTemperature.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SmartAquaTemperature newInstance(String param1, String param2) {
-        SmartAquaTemperature fragment = new SmartAquaTemperature();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view;
+        view = inflater.inflate(R.layout.fragment_smart_aqua_temperature,container, false);
+        seekBar = view.findViewById(R.id.SmartAquaTempSeekBar);
+        textView = view.findViewById(R.id.SmartAquaTempReading3);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                textView.setText(String.valueOf(progress));
 
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            view = inflater.inflate(R.layout.fragment_smart_aqua_temperature,container, false);
-        }
-        else {
-            view = inflater.inflate(R.layout.fragment_smart_aqua_temperature, container, false);
-        }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         return view;
     }
 }
