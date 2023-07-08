db1b73709bb13a0347a8dd9949bbc443e19e4895
pquezon <n01424883@humbermail.ca>
2023-07-07 23:00:58 -0400

    Implemented notification functionality

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperature.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperature.java
@@ -6,17 +6,29 @@
 
 package ca.buckleupinc.it.smartaqua;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.annotation.SuppressLint;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 public class SmartAquaTemperature extends Fragment {
-    SeekBar seekBar;
-    TextView textView;
+    private static final String CHANNEL_ID = "TemperatureNotificationChannel";
+    private SeekBar seekBar;
+    private TextView textView;
 
     public SmartAquaTemperature() {
         // Required empty public constructor
@@ -25,13 +37,11 @@ public class SmartAquaTemperature extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view;
-        view = inflater.inflate(R.layout.fragment_smart_aqua_temperature,container, false);
+        View view = inflater.inflate(R.layout.fragment_smart_aqua_temperature, container, false);
         seekBar = view.findViewById(R.id.SmartAquaTempSeekBar);
         textView = view.findViewById(R.id.SmartAquaTempReading3);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 textView.setText(String.valueOf(progress));
@@ -45,6 +55,49 @@ public class SmartAquaTemperature extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+        ToggleButton toggleButton = view.findViewById(R.id.SmartAquaTempToggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message = isChecked ? "Temperature turned on" : "Temperature turned off";
+                displayNotification(message);
+            }
+        });
+
         return view;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void displayNotification(String message) {
+        Context context = requireContext();
+
+        createNotificationChannel();
+
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle("Temperature Status")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(1, notification);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Temperature Channel";
+            String description = "Channel for Temperature Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+
+            NotificationManager notificationManager = requireContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
