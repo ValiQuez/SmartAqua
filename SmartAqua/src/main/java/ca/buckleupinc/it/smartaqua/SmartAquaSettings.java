45e8715dd9e3865a5154922cccb49b4763fe532b
pquezon <n01424883@humbermail.ca>
2023-07-09 18:50:21 -0400

    Implemented location runtime location and logic to display current location

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaSettings.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaSettings.java
@@ -3,32 +3,42 @@
     Alvaro Rodrigo Chavez Moya - N01455107
     Paolo Adrian Quezon - N01424883
     Nicholas Dibiase - N01367109            */
+
 package ca.buckleupinc.it.smartaqua;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioManager;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.google.android.material.snackbar.Snackbar;
-import android.Manifest;
-
 public class SmartAquaSettings extends Fragment {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,34 +72,7 @@ public class SmartAquaSettings extends Fragment {
 
         //=====LOCATION USER PERMISSION REQUEST=====
         Button btn = view.findViewById(R.id.SmartAquaUsrPermBtn);
-        btn.setOnClickListener(view1 -> {
-
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle(getResources().getString(R.string.settingsPermissionTitle));
-                builder.setMessage(getResources().getString(R.string.settingsPermissionBody));
-                builder.setIcon(R.drawable.location_icon);
-                builder.setPositiveButton(R.string.settingsPermissionGrant, (dialogInterface, i) -> {
-
-                    //Toast.makeText(getActivity(),R.string.settingsAccessGranted, Toast.LENGTH_SHORT).show();
-                    Snackbar grantedBar = Snackbar.make(view1, R.string.settingsAccessGranted, Snackbar.LENGTH_SHORT);
-                    grantedBar.show();
-                    Uri link = Uri.parse("http://maps.google.com/maps");
-                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, link);
-                    mapIntent.setPackage("com.google.android.apps.maps");
-                    startActivity(mapIntent);
-                });
-
-                builder.setNegativeButton(R.string.settingsPermissionDeny, (dialogInterface, i) -> {
-                    //Toast.makeText(getActivity(),R.string.settingsAccessDenied, Toast.LENGTH_SHORT).show();
-                    Snackbar deniedBar = Snackbar.make(view1, R.string.settingsAccessDenied, Snackbar.LENGTH_SHORT);
-                    deniedBar.show();
-                });
-
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
-        });
+        btn.setOnClickListener(view1 -> requestLocationPermission());
 
         //=====MUTE APP=====
         AudioManager aManager = (AudioManager)getActivity().getSystemService(Context.AUDIO_SERVICE);
@@ -112,6 +95,114 @@ public class SmartAquaSettings extends Fragment {
         });
 
         return view;
+    }
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                showPermissionExplanationDialog();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            // Permission already granted, displays current location
+            displayCurrentLocation();
+        }
+    }
+
+    private void showPermissionExplanationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getResources().getString(R.string.settingsPermissionTitle));
+        builder.setMessage(getResources().getString(R.string.settingsPermissionBody));
+        builder.setIcon(R.drawable.location_icon);
+        builder.setPositiveButton(R.string.settingsPermissionGrant, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Request location permission
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton(R.string.settingsPermissionDeny, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Permission denied, do nothing
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void displayCurrentLocation() {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Display the current location
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String currentLocation = "Latitude: " + latitude + "\nLongitude: " + longitude;
+                Toast.makeText(getActivity(), currentLocation, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Check if location services are enabled
+        if (isLocationEnabled()) {
+            // Request location updates
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            }
+        } else {
+            // Location services are disabled, open settings to enable them
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        return isGpsEnabled || isNetworkEnabled;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, display current location
+                displayCurrentLocation();
+            } else {
+                // Location permission denied, do nothing
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null && locationListener != null) {
+            locationManager.removeUpdates(locationListener);
+        }
     }
 
 
