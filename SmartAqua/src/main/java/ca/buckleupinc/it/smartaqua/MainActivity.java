7c49d36464d6895fc53ae2e3a54d1d69306e031b
AlvaroChavez5017 <rodrigochv1605@gmail.com>
2023-07-13 17:44:03 -0400

    Implemented shared preferences for the dark mode in the main activity

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/MainActivity.java
@@ -10,11 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -42,6 +45,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        SharedPreferences sharedPref = getSharedPreferences("UserPrefSettings", Context.MODE_PRIVATE);
+        boolean darkModeEnabled = sharedPref.getBoolean("DarkModeToggleState", false);
+        if(darkModeEnabled){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
 
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.SmartAquaFragmentSection, new SmartAquaHome()).commit();
