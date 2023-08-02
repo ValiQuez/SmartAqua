d9932eee86a89c4ff8e32d085cb8c319e6ba7962
AlvaroChavez5017 <rodrigochv1605@gmail.com>
2023-08-01 23:09:02 -0400

    Added the functionality for the SmartAquaQuality screen when it is offline

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaQuality.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaQuality.java
@@ -6,12 +6,10 @@
 
 package ca.buckleupinc.it.smartaqua;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +17,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -38,6 +38,10 @@ public class SmartAquaQuality extends Fragment {
     private Runnable runnable;
     private DatabaseReference dbRef;
     private List<Integer> tdsDataList;
+    private boolean isOnline;
+    private int lastDisplayedIndex = -1;
+    private SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_KEY = "SmartAquaQualityData";
 
     public SmartAquaQuality() {
 
@@ -48,43 +52,42 @@ public class SmartAquaQuality extends Fragment {
         super.onCreate(savedInstanceState);
 
         handler = new Handler();
+        tdsDataList = new ArrayList<>();
+        sharedPreferences = requireContext().getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE);
+
         runnable = new Runnable() {
             @Override
             public void run() {
-                int randomIndex = new Random().nextInt(tdsDataList.size());
-                int reading_ran = tdsDataList.get(randomIndex);
-                readings_TDS.setText(String.valueOf(reading_ran));
-                //int reading_ran = new Random().nextInt(201)+300;
-                readings_TDS.setText(String.valueOf(reading_ran));
-
-                if (reading_ran >= 390 && reading_ran <= 460) {
-                    status_TDS.setText(R.string.s_good);
-                    status_TDS.setTextColor(Color.GREEN);
+                if (isOnline && !tdsDataList.isEmpty()) {
+                    // Data is available from Firebase and online, show random readings
+                    int randomIndex = new Random().nextInt(tdsDataList.size());
+                    int reading_ran = tdsDataList.get(randomIndex);
+                    displayReading(reading_ran);
                 } else {
-                    status_TDS.setText(R.string.s_bad);
-                    status_TDS.setTextColor(Color.RED);
+                    // No data available from Firebase or offline, display the last cached readings in a loop
+                    if (!tdsDataList.isEmpty()) {
+                        lastDisplayedIndex = (lastDisplayedIndex + 1) % tdsDataList.size();
+                        int reading_ran = tdsDataList.get(lastDisplayedIndex);
+                        displayReading(reading_ran);
+                    }
                 }
 
-                handler.postDelayed(this,5000);
+                handler.postDelayed(this, 5000);
             }
         };
-        tdsDataList = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view;
-
-        view = inflater.inflate(R.layout.fragment_smart_aqua_quality, container, false);
+        View view = inflater.inflate(R.layout.fragment_smart_aqua_quality, container, false);
 
         readings_TDS = view.findViewById(R.id.SmartAquaWaterReadings);
         status_TDS = view.findViewById(R.id.SmartAquaWStatus);
 
         Button dbButton = view.findViewById(R.id.SmartAquaWQButton);
 
-        dbButton.setOnClickListener(view1 ->{
+        dbButton.setOnClickListener(view1 -> {
             String reading_TDS_str = readings_TDS.getText().toString();
             String status_TDS_str = status_TDS.getText().toString();
 
@@ -94,6 +97,7 @@ public class SmartAquaQuality extends Fragment {
             Toast.makeText(getActivity(), R.string.save_data, Toast.LENGTH_SHORT).show();
         });
 
+        setupNetworkConnectivityListener();
         readDataFromDatabase();
 
         return view;
@@ -102,16 +106,20 @@ public class SmartAquaQuality extends Fragment {
     private void readDataFromDatabase() {
         DatabaseReference tdsDataRef = FirebaseDatabase.getInstance().getReference("TDS_DATA");
 
-        tdsDataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        tdsDataRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tdsDataList.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     int tdsData = snapshot.getValue(Integer.class);
                     tdsDataList.add(tdsData);
                 }
+                // Save data to SharedPreferences for offline caching
+                saveDataToSharedPreferences();
                 // Start displaying random TDS data
                 handler.post(runnable);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle any errors that occur during data reading
@@ -120,9 +128,62 @@ public class SmartAquaQuality extends Fragment {
         });
     }
 
+    private void saveDataToSharedPreferences() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        StringBuilder dataStringBuilder = new StringBuilder();
 
+        for (int tdsData : tdsDataList) {
+            dataStringBuilder.append(tdsData).append(",");
+        }
 
-    public void onPause(){
+        editor.putString("tdsDataList", dataStringBuilder.toString());
+        editor.apply();
+    }
+
+    private void loadDataFromSharedPreferences() {
+        String dataString = sharedPreferences.getString("tdsDataList", "");
+        String[] dataValues = dataString.split(",");
+
+        tdsDataList.clear();
+        for (String value : dataValues) {
+            if (!value.isEmpty()) {
+                tdsDataList.add(Integer.parseInt(value));
+            }
+        }
+    }
+
+    private void setupNetworkConnectivityListener() {
+        DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                isOnline = snapshot.getValue(Boolean.class);
+                if (!isOnline) {
+                    // If offline, load cached data from SharedPreferences
+                    loadDataFromSharedPreferences();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle network connectivity error
+            }
+        });
+    }
+
+    private void displayReading(int reading_ran) {
+        readings_TDS.setText(String.valueOf(reading_ran));
+        if (reading_ran >= 390 && reading_ran <= 460) {
+            status_TDS.setText(R.string.s_good);
+            status_TDS.setTextColor(Color.GREEN);
+        } else {
+            status_TDS.setText(R.string.s_bad);
+            status_TDS.setTextColor(Color.RED);
+        }
+    }
+
+    @Override
+    public void onPause() {
         super.onPause();
         handler.removeCallbacks(runnable);
     }
