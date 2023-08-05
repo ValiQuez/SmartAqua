1dac97b2f2730f0f2c99fb7ec9852fa7a1096fe6
pquezon <n01424883@humbermail.ca>
2023-08-05 19:30:17 -0400

    Protoyped the  2 invalid test cases for Robletric unit testing

--- SmartAqua/src/test/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperatureTest.java
+++ SmartAqua/src/test/java/ca/buckleupinc/it/smartaqua/SmartAquaTemperatureTest.java
@@ -1,18 +1,10 @@
-/*  CENG-322-0NA: Group 6
-    Denis Shwaloff - N01422583
-    Alvaro Rodrigo Chavez Moya - N01455107
-    Paolo Adrian Quezon - N01424883
-    Nicholas Dibiase - N01367109            */
-
 package ca.buckleupinc.it.smartaqua;
 
 import android.os.Build;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,42 +12,40 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(RobolectricTestRunner.class)
-@Config(sdk = {Build.VERSION_CODES.O_MR1}) // Specify the SDK version
+@Config(sdk = Build.VERSION_CODES.O_MR1)
 public class SmartAquaTemperatureTest {
 
     private FragmentActivity activity;
 
     @Before
     public void setUp() {
-        activity = Robolectric.buildActivity(FragmentActivity.class).create().get();
+        activity = Robolectric.buildActivity(FragmentActivity.class).create().start().resume().get();
     }
 
-    // Valid Test Case 1
-    @Test
-    public void testSeekBarProgressAndTemperatureRange() {
-        activity.getSupportFragmentManager().beginTransaction()
-                .add(new SmartAquaTemperature(), null)
-                .commitNow();
+    // Invalid Test Case 1
+    @Test(expected = NullPointerException.class)
+    public void testSeekBarProgressAndTemperatureRange_invalid_nullView() {
+        SmartAquaTemperature fragment = new SmartAquaTemperature();
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        transaction.add(fragment, null).commit();
 
-        Fragment scenario = activity.getSupportFragmentManager().findFragmentById(R.id.SmartAquaTempLayout);
-
-        SeekBar seekBar = scenario.getView().findViewById(R.id.SmartAquaTempSeekBar);
-        TextView textView = scenario.getView().findViewById(R.id.SmartAquaTempReading3);
-
-        // Set seekBar progress to 50
-        seekBar.setProgress(50);
-
-        // Calculate the expected temperature range based on the formula used in the fragment
-        int expectedTemperatureRange = (int) (50 * 0.09) + 18;
-
-        // Construct the expected temperature text
-        String expectedTemperatureText = expectedTemperatureRange + scenario.getString(R.string.tempCelcius);
-
-        // Verify if the displayed temperature text matches the expected value
-        assertEquals(expectedTemperatureText, textView.getText().toString());
+        SeekBar seekBar = fragment.getView().findViewById(R.id.SmartAquaTempSeekBar);
+        // Attempting to access the view of a fragment before its onCreateView() is called
+        // This should throw a NullPointerException
     }
 
+    // Invalid Test Case 2
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidSeekBarProgress() {
+        SmartAquaTemperature fragment = new SmartAquaTemperature();
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        transaction.add(fragment, null).commit();
+
+        SeekBar seekBar = fragment.getView().findViewById(R.id.SmartAquaTempSeekBar);
+
+        // Attempting to set progress beyond the maximum value (100)
+        seekBar.setProgress(120);
+        // This should throw an IllegalArgumentException as it's an invalid progress value
+    }
 }
