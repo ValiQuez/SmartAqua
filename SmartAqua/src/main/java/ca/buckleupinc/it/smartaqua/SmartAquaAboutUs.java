5ae814030b1177d00415c2e6afc16945f626b17a
AlvaroChavez5017 <rodrigochv1605@gmail.com>
2023-08-05 22:12:57 -0400

    Reformatted different classes: making Strings to static final and deleting spaces.

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaAboutUs.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaAboutUs.java
@@ -19,15 +19,13 @@ public class SmartAquaAboutUs extends Fragment {
     public SmartAquaAboutUs() {
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_smart_aqua_about_us, container, false);
     }
