a9c72070b1d66a29081cfb0110a01d54b0a6348a
pquezon <n01424883@humbermail.ca>
2023-08-05 22:24:24 -0400

    Nicholas Dibiase - Reformatted code in SmartAquaDarkmode, updated formatting

--- SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaDarkMode.java
+++ SmartAqua/src/main/java/ca/buckleupinc/it/smartaqua/SmartAquaDarkMode.java
@@ -17,6 +17,7 @@ public class SmartAquaDarkMode implements CompoundButton.OnCheckedChangeListener
 
     private final ToggleButton darkTB;
     private final SharedPreferences settingsPreferences;
+    private static final String DARK_MODE_TOGGLE_STATE_KEY = "DarkModeToggleState";
 
     public SmartAquaDarkMode(ToggleButton darkTB, SharedPreferences settingsPreferences) {
         this.darkTB = darkTB;
@@ -26,14 +27,14 @@ public class SmartAquaDarkMode implements CompoundButton.OnCheckedChangeListener
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         // Check if the state has actually changed
-        if (isChecked != settingsPreferences.getBoolean("DarkModeToggleState", false)) {
+        if (isChecked != settingsPreferences.getBoolean(DARK_MODE_TOGGLE_STATE_KEY, false)) {
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 Toast.makeText(buttonView.getContext(), R.string.darkModeON, Toast.LENGTH_SHORT).show();
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
-            settingsPreferences.edit().putBoolean("DarkModeToggleState", isChecked).apply();
+            settingsPreferences.edit().putBoolean(DARK_MODE_TOGGLE_STATE_KEY, isChecked).apply();
         }
     }
 }
